396f2bc87f1b4ea408121437c7616418d7033426
sarahAbuhaimed <sar-abu@hotmail.com>
2019-03-13 10:41:37 +0300

    remove off click and add score

--- Goal.js
+++ Goal.js
@@ -1,16 +1,5 @@
 
-//drag and drop img of football tems inside plyer div
-function allowDrop(event) {
-    event.preventDefault();
-}
-function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
-}
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
-}
+
 //object with all cases plyer can win by
 var winCase = {
     1: ["square0", "square1", "square2"],
@@ -22,85 +11,90 @@ var winCase = {
     7: ["square0", "square4", "square8"],
     8: ["square0", "square3", "square6"]
 }
+
 //plyers variable store array of divs selected
 var player1 = [];
 var player2 = [];
+var plyer1Score = 0;
+var plyer2Score = 0;
 //to store sign x and O
 var sign = true;
+
 //function for button which reload page 
 $('.buttonNew').on("click", function (event) {
     location.reload();
 });
+
 // highlight the mouseover divs
 $('.square').on("mouseover", function (event) {
-
     $(this).addClass("black");
-
 });
+
 // remove highlight the mouseover divs
 $('.square').on("mouseout", function (event) {
-
     $(this).removeClass("black");
 });
 
 //on click boxs to change from x to o
 $('.square').on("click", function (event) {
     $(this).off("mouseout");
+
+    //on click boxs become X 
     if (sign == true) {
         $(this).text("X");
         sign = !sign;
         player1.push(event.target.id);
-        //
-        console.log(player1);
-    } else {
+    }
+
+    //on click boxs become O
+     else {
         $(this).text("O");
         sign = !sign;
         player2.push(event.target.id);
-        //event.target.id
-        console.log(player2);
-
     }
     checkWinner()
 })
 
 //fuction to check if player1 or plyer2 is win
 function checkWinner() {
+
     var noWon = true;
     for (var i = 1; i < 9; i++) {
+
         if (player1.includes(winCase[i][0]) && player1.includes(winCase[i][1]) && player1.includes(winCase[i][2])) {
             noWon = false;
-               //to stop plyer from clicking another box after wining
-            $('.square').off('click');
 
             //stop background audio 
             document.getElementById('stop').pause();
             //play success audio
             document.getElementById('goal').play();
+
+            //alert message for wining
             swal({
                 title: "Team 1 Win",
                 icon: "img/Goal1.png"
-            //     onClose: closePlay
-
-            // }).then(function closePlay() {
-            //     return document.getElementById('goal').pause();
-            
+              
             });
-        } 
+                        //get plyer1 score
+                        plyer1Score += 1;
+                        document.getElementById('score0').textContent = plyer1Score;
+        }
 
         else if (player2.includes(winCase[i][0]) && player2.includes(winCase[i][1]) && player2.includes(winCase[i][2])) {
             noWon = false;
-            //to stop plyer from clicking another box after wining
-            $('.square').off('click');
-              //stop background audio 
-              document.getElementById('stop').pause();
-           //play success audio
+            //stop background audio 
+            document.getElementById('stop').pause();
+            //play success audio
             document.getElementById('goal').play();
+
             swal({
-                 
-                title: "Team 1 Win",
+
+                title: "Team 2 Win",
                 icon: "img/Goal1.png",
             });
-           
+            //get plyer1 score
+            plyer2Score += 1;
+            document.getElementById('score1').textContent = plyer2Score;
         }
 
         //if no one wins 
@@ -117,10 +111,30 @@ function checkWinner() {
             }
         }
     }
-    
-}  
 
+}
+function secondHalf(){
+    $(".square").text("");
+    player1 = []
+    player2 = []
+    // remove highlight the mouseover divs
+$('.square').on("mouseout", function (event) {
+    $(this).removeClass("black");
+});
+}
 
+//drag and drop img of football teams inside plyer div
+function allowDrop(event) {
+    event.preventDefault();
+}
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
 
 
 
